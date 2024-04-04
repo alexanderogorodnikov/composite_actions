9e3c4f1474227b9ed045be2cf4c7a568da6d74d4
alexanderogorodnikov <ogorodnikov.alexander@gmail.com>
2024-04-04 17:07:35 -0400

    added custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,17 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+ //1) Get input values from workflow file
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion  = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // github.context.payload
+  // 2) Upload my files to S3 bucket
+  const s3Uri = `s3://${bucket}`;
+
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice('HEllo fron my cutom java-sctipt action');
 }
 
